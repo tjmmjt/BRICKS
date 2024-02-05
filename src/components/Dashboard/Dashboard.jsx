570aaf3cf9fecb84cb97577a2c1cb63e3e39524f
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-02-05 14:37:29 -0600

    created <Dashboard /> to be displayed on <UserPage />, passed username and id as props. functional

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+
+
+function Dashboard(props) {
+
+    return (
+        <>
+            <h2>Welcome, {props.user.username}!</h2>
+            <p>Your ID is: {props.user.id}</p>
+        </>
+    )
+}
+
+export default Dashboard
