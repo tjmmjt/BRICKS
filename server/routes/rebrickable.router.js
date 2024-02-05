d76039bee940ac10e47f02dd9196294b0aa95f7c
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-02-05 14:55:58 -0600

    rebrickable.router tested with postman and functioning with .env API key and static data. Next, add :id param and test with postman

--- server/routes/rebrickable.router.js
+++ server/routes/rebrickable.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const { default: axios } = require('axios');
 const router = express.Router();
 
 /**
@@ -7,6 +8,19 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route code here
+  // get api data
+  // start with static code to test with postman
+    const apiKey=process.env.REBRICKABLE_KEY
+
+    axios.get(`https://rebrickable.com/api/v3/lego/sets/40649-1?key=${apiKey}`)
+    .then(response => {
+        console.log(response.data)
+        res.send(response.data)
+    })
+    .catch(err => {
+        console.error('Error getting Rebrickable Request:', err)
+        res.sendStatus(500)
+    })
 });
 
 /**
